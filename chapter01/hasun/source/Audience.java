fd00cb9d8ca2630aba2c11ed7f7f669a44b581e8
hasunzo <gktjsdl470@gmail.com>
2022-07-13 18:04:19 +0900

    chapter01 객체, 설계

---
+++ chapter01/hasun/source/Audience.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        return bag.hold(ticket);
+    }
+}
---
+++ chapter01/hasun/source/Bag.java
@@ -0,0 +1,47 @@
+package chapter1;
+
+public class Bag {
+    private Long amount;
+    public Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
+    private boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return this.ticket != null;
+    }
+
+    private void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+}
